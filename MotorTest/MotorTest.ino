74d5df5f2d1de29633260037f7b13de07c665c54
Marion Madanguit <mmadanguit@olin.edu>
2021-10-14 13:09:20 -0400

    simple feedback loop

--- MotorTest/MotorTest.ino
+++ MotorTest/MotorTest.ino
@@ -4,38 +4,75 @@
 Adafruit_MotorShield AFMS = Adafruit_MotorShield();
 
 // Select which 'port' M1, M2, M3 or M4. In this case, M1
-Adafruit_DCMotor *myMotor = AFMS.getMotor(1);
-Adafruit_DCMotor *myMotor2 = AFMS.getMotor(2);
+Adafruit_DCMotor *rMotor = AFMS.getMotor(1);
+Adafruit_DCMotor *lMotor = AFMS.getMotor(2);
+
+// Analog input pins that the distance sensors are attached to
+const int rAnalogPin = A0;  
+const int lAnalogPin = A1; 
+
+// Value reads from distance sensors
+int rSensorValue = 0;        
+int lSensorValue = 0;       
 
 uint32_t start_time;
 
-void setup() {
-  Serial.begin(9600);           // set up Serial library at 9600 bps
+const int highSpeed = 50;
+const int lowSpeed = 25;
 
-  if (!AFMS.begin()) {         // create with the default frequency 1.6KHz
+
+void setup() {
+  // Initialize serial communications at 9600 bps:
+  Serial.begin(9600);           
+
+  // Check if the motor shield has been found
+  if (!AFMS.begin()) {        
     Serial.println("Could not find Motor Shield. Check wiring.");
     while (1);
   }
   Serial.println("Motor Shield found.");
 
   // Set the speed to start, from 0 (off) to 255 (max speed)
-  myMotor->setSpeed(100);
-  myMotor2->setSpeed(100);  
+  rMotor->setSpeed(highSpeed);
+  lMotor->setSpeed(highSpeed);  
 
   start_time = millis();
 }
+
 
 void loop() {  
   uint32_t current_time;
   current_time = millis();
 
-  while (current_time < start_time + 1000) {
+  while (current_time < start_time + 1000000) {
+    
       Serial.println("Entered loop");
-      myMotor->run(FORWARD);
-      myMotor2->run(FORWARD);
+      
+      rMotor->run(FORWARD);
+      lMotor->run(FORWARD);
       current_time = millis();
+
+      rSensorValue = analogRead(rAnalogPin);
+      lSensorValue = analogRead(lAnalogPin);
+
+      // Decrease right motor if right sensor goes on tape
+      if (rSensorValue > 995) {
+        rMotor->setSpeed(lowSpeed);
+      } 
+      else if (rSensorValue <= 995) {
+        rMotor->setSpeed(highSpeed);
+      }
+
+      // Decrease left motor if left sensor goes on tape
+      if (lSensorValue > 995) {
+        lMotor->setSpeed(lowSpeed);
+      } 
+      else if (lSensorValue <= 995) {
+        lMotor->setSpeed(highSpeed);
+      }
+      
   }
 
-  myMotor->run(RELEASE);
-  myMotor2->run(RELEASE);
+  rMotor->run(RELEASE);
+  lMotor->run(RELEASE);
 }
